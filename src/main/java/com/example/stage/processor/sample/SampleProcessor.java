13572f20ee95858d0492a95c0b2c14141f4fcc05
vopolskiy <vopolskiy@luxoft.com>
2020-03-23 00:30:08 +0300

    Changed config

--- src/main/java/com/example/stage/processor/sample/SampleProcessor.java
+++ src/main/java/com/example/stage/processor/sample/SampleProcessor.java
@@ -1,36 +1,11 @@
-/**
- * Copyright 2015 StreamSets Inc.
- *
- * Licensed under the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.stage.processor.sample;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
 import com.example.stage.lib.sample.Errors;
 
 import com.streamsets.pipeline.api.Field;
-import com.streamsets.pipeline.api.FileRef;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.StageException;
-import com.streamsets.pipeline.api.base.OnRecordErrorException;
 import com.streamsets.pipeline.api.base.SingleLaneRecordProcessor;
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
@@ -55,6 +30,7 @@ public abstract class SampleProcessor extends SingleLaneRecordProcessor {
   public RedissonClient redisson;
 
   /** {@inheritDoc} */
+  @SuppressWarnings("unchecked")
   @Override
   protected List<ConfigIssue> init() {
     // Validate configuration values and open any required resources.
@@ -72,7 +48,11 @@ public abstract class SampleProcessor extends SingleLaneRecordProcessor {
       LOG.info("Init  after validate:");
 
       Config config = new Config();
-      config.useSingleServer().setAddress("redis://redis:6379");
+      config
+          .useSingleServer()
+          .setAddress("redis://redis:6379")
+          .setConnectTimeout(50000);
+
       redisson = Redisson.create(config);
 
       LOG.info("Init  rediss config init1:");
@@ -85,12 +65,13 @@ public abstract class SampleProcessor extends SingleLaneRecordProcessor {
   /** {@inheritDoc} */
   @Override
   public void destroy() {
+      super.destroy();
+
       if (redisson != null)
           redisson.shutdown();
 
       LOG.info("!!!!!!   Test Destroy ");
     // Clean up any open resources.
-    super.destroy();
   }
 
   /** {@inheritDoc} */
